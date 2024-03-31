5d668dfe6afd0239edb84d8044eb4469adff594b
Readyornot_YOU <2285813721@qq.com>
2024-03-31 15:50:47 +0800

    fix

--- SqlConnect.php
+++ SqlConnect.php
@@ -9,7 +9,7 @@ $dbname = "php_db";
 // 数据库连接本地
 $conn = new mysqli($servername, $username, $password, $dbname,);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if ($conn->connect_error) {// 检查连接
+    die("Connection failed: " . $conn->connect_error); 
 }
 
